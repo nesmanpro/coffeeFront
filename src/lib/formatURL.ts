7ab7d5b226538a814d8f80426ae213c3842a6b39
nesmanpro <nesmanpro@gmail.com>
2025-04-22 16:29:46 +0200

    type errors

--- src/lib/formatURL.ts
+++ src/lib/formatURL.ts
@@ -1,6 +1,6 @@
 
 
-export default function formatURL(IMG_URL) {
+export default function formatURL(IMG_URL: string) {
     const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
     return (API_URL + IMG_URL)
